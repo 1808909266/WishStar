a1539881278b8b4a4ef6ddd5bb95b4b8d76d8c16
TongXin <1808909266@qq.com>
2024-11-20 02:21:36 +0800

    第二次提交

---
+++ app/src/main/java/com/example/constellation/activity/ChangePSWActivity.java
@@ -0,0 +1,14 @@
+package com.example.constellation.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChangePSWActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_pswactivity);
+    }
+}
